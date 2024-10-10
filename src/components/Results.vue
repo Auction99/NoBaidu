05ddae86a84175834889ff90741d60eaa98e43d5
Auction99 <134117814+Auction99@users.noreply.github.com>
2024-10-10 10:16:54 +0800

    Update Results.vue

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-container">
     <div class="search-container" id="searchContainer">
-      <h1 class="search-title" @click="goHome">Luxirty Search</h1>
+      <h1 class="search-title" @click="goHome">BeyBaidu</h1>
       <div class="gcse-searchbox"></div>
     </div>
     <div class="search-result-zone">
@@ -34,7 +34,7 @@ export default {
     },
     setTitle() {
       var inputContent = document.getElementsByName('search')[0].value;
-      document.title = inputContent + ' - Luxirty Search'
+      document.title = inputContent + ' - BeyBaidu'
     },
     goHome() {
       // 使用 window.location.href 跳转到根路径
